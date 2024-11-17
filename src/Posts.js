0ad2be469691d1e938a50fa5aa7d313735e7c377
lucasazevedo98 <azevedi22@gmail.com>
2024-11-17 18:35:43 -0300

    curtida via post

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,4 @@
-import React from 'react'
-
-
+import { useState } from "react"
 
 
 export default function Posts() {
@@ -40,32 +38,87 @@ export default function Posts() {
     ]
 
     return (
-        <div class="post">
-            {/* Topo */}
-            {post.map((topo)=>(
-                <Topo foto={topo.usuario_foto} nome={topo.usuario_nome}/>
+        <div class="posts">
+
+            {post.map((post) => (
+                <Post
+                    usuario_foto={post.usuario_foto}
+                    usuario_nome={post.usuario_nome}
+
+                    post_foto={post.post_foto}
+                    post_nome={post.post_nome}
+
+
+
+                />
             ))}
+
         </div>
+    )
+}
+function Post(props) {
+    const { usuario_foto, usuario_nome, post_foto, post_nome } = props
+    return (
+        <div class="post">
+            <Topo usuario_foto={usuario_foto} usuario_nome={usuario_nome} />
 
+            <Conteudo post_foto={post_foto} post_nome={post_nome} />
 
-
+        </div>
     )
 }
 
-
 function Topo(props) {
-    const {foto,nome} = props
+    const { usuario_foto, usuario_nome } = props
     return (
-
         <div class="topo">
             <div class="usuario">
-                <img src={foto} alt={nome} />
+                <img src={usuario_foto} alt={usuario_nome} />
                 meowed
             </div>
             <div class="acoes">
                 <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
         </div>
-
     )
 }
+
+function Conteudo(props) {
+    const { post_foto, post_nome } = props
+
+    const [like,setLike] = useState('heart-outline')
+    const [cor,setCor] = useState('')
+
+    function curtir() {
+        setLike("heart-sharp")
+        setCor("red")
+    }
+    return (
+        <>
+            <div class="conteudo">
+                <img src={post_foto} alt={post_nome} onClick={curtir}/>
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon style={{color: cor}} name={like}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src="assets/img/respondeai.svg" alt="respondeai" />
+                    <div class="texto">
+                        Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+

46335ce266f3a3740f541c3b75b9a216ec38bd2b
lucasazevedo98 <azevedi22@gmail.com>
2024-11-18 09:05:33 -0300

    adicionei a funcao de mudar nome clicando no botao

--- src/User.js
+++ src/User.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 export default function User() {
 
     const [foto,setFoto] = useState('assets/img/catanacomics.svg')
+    const [nome,setNome] = useState('catanacomics')
 
     function mudarFoto() {
         let pergunta = prompt("insira a foto que seja:")
@@ -15,14 +16,24 @@ export default function User() {
 
     }
 
+    function mudarNome(){
+        let alterarNome = prompt("insira o nome que seja:")
+        setNome(alterarNome)
+
+        if(alterarNome === null || alterarNome ==="")
+            {
+                setNome(nome)
+            }
+    }
+
 
     return (
         <div class="usuario">
             <img src={foto} alt="imagem de perfil" onClick={mudarFoto} />
             <div class="texto">
-                <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                <span >
+                    <strong>{nome}</strong>
+                    <ion-icon name="pencil" onClick={mudarNome}></ion-icon>
                 </span>
             </div>
         </div>
